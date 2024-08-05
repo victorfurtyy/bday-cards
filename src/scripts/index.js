ea2d969dcbe2946b8cad95929761162527eeb0be
victorfurtyy <victorfurtyy@gmail.com>
2024-08-05 19:27:20 -0300

    errata; favicon correction

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,10 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
+if (process.env.NODE_ENV === 'development') document.head.title = 'B-DAY Photo | DEV';
+
+
+
 const swiper = new Swiper('.swiper', {
     effect: 'flip',
     loop: true,
