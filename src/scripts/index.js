5a8a4d91861ff391303d9325a1087f2857d044d1
victorfurtyy <victorfurtyy@gmail.com>
2024-08-06 13:58:51 -0300

    feat: Add modal dialog and disable hint on active index change

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,12 +8,23 @@ if (process.env.NODE_ENV === 'development') document.title = 'B-DAY Photo | DEV'
 const swiper = new Swiper('.swiper', {
     effect: 'flip',
     loop: true,
+    on: {
+        activeIndexChange: function () {
+            if (this.activeIndex === 1) 
+                disableHint();
+        }
+    }
 });
+window.swiper = swiper;
 
 
 function disableHint() {
-    swiper.el.classList.toggle('hint');
+    swiper.el.classList.remove('hint');
 }
 
 
-swiper.on('slideChange', disableHint);
+// REMOVE
+function toggleHint() {
+    swiper.el.classList.toggle('hint');
+}
+window.toggleHint = toggleHint;
