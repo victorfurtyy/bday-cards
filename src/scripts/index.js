5de7a75ce7307fffdad39d43f57dc78e6c43d705
victorfurtyy <victorfurtyy@gmail.com>
2024-08-06 17:58:25 -0300

    feat: Update modal dialog and favicon
    - Add new features to the modal dialog, including header, content, and footer sections
    - Update the favicon image URL to a new location

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,30 @@ import 'swiper/css/bundle';
 if (process.env.NODE_ENV === 'development') document.title = 'B-DAY Photo | DEV';
 
 
+function openModal(modalId) {
+    const modal = document.querySelector('.modal#'+modalId);
+    modal.classList.add('open');
+    modal.showModal();
+}
+window.openModal = openModal;
+
+function closeModal(modalId) {
+    const modal = document.querySelector('.modal#'+modalId);
+    modal.classList.remove('open');
+
+    const duration = Number(getComputedStyle(modal).getPropertyValue('--modal-transition').replace('s', ''))*1000
+    setTimeout(() => modal.close(), duration);
+}
+window.closeModal = closeModal;
+
+function toggleModal(modalId) {
+    const modal = document.querySelector('.modal');
+    if (modal.classList.contains('open')) closeModal(modalId);
+    else openModal(modalId);
+}
+window.toggleModal = toggleModal;
+
+
 
 const swiper = new Swiper('.swiper', {
     effect: 'flip',
