2856733e64994feb1d405a5373a4b629d0508c68
victorfurtyy <victorfurtyy@gmail.com>
2024-08-10 01:57:02 -0300

    asdf

--- src/scripts/users.js
+++ src/scripts/users.js
@@ -7,4 +7,72 @@ export const USERS = {
         desc: "Ana Beatriz",
         img: ""
     },
+    "norman": {
+        desc: "Norman",
+        img: "https://www.dropbox.com/scl/fi/a0o41wa4g5k8kbeu5gbqo/6b412ad1-3d30-4487-9f3e-f57bc6a42ffd.jpeg?rlkey=ootidccg4sh6trdn3sjj1yqio&st=nk5kj7ck&raw=1"
+    },
+    "furtadoirlene": {
+        desc: "Irlene Furtado",
+        img: ""
+    },
+    "antoniojorge10_": {
+        desc: "Antônio Jorge",
+        img: ""
+    },
+    "leticiafeba_": {
+        desc: "Lê",
+        img: ""
+    },
+    "_manu_marques": {
+        desc: "Manu",
+        img: ""
+    },
+    "anarfls": {
+        desc: "Ana Frozenha",
+        img: ""
+    },
+    "jdeliciaa": {
+        desc: "Jota Delícia",
+        img: ""
+    },
+    "josifurtado6": {
+        desc: "Joyce",
+        img: ""
+    },
+    "mariane": {
+        desc: "Mariane",
+        img: ""
+    },
+    "lud": {
+        desc: "Ludimila",
+        img: ""
+    },
+    "maju": {
+        desc: "Maju",
+        img: ""
+    },
+    "arthur": {
+        desc: "Arthur",
+        img: ""
+    },
+    "joanadarcfurtado_": {
+        desc: "Joana Darc",
+        img: ""
+    },
+    "irli_montelo": {
+        desc: "Irliane",
+        img: ""
+    },
+    "inethypereira": {
+        desc: "Inethy",
+        img: ""
+    },
+    "sunvitoria": {
+        desc: "Luiza",
+        img: ""
+    },
+    "victtineo": {
+        desc: "Lorenzo",
+        img: ""
+    }
 }
