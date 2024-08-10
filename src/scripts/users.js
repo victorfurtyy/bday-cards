00424a209788f65fb0eeb4dfc6292291795bf778
victorfurtyy <victorfurtyy@gmail.com>
2024-08-10 01:29:04 -0300

    chore: Add new users to the user list and update modal dialog styles

--- src/scripts/users.js
+++ src/scripts/users.js
@@ -7,4 +7,72 @@ export const USERS = {
         desc: "Ana Beatriz",
         img: ""
     },
+    "norman": {
+        desc: "Norman",
+        img: ""
+    },
+    "furtadoirlene": {
+        desc: "Irlene Furtado",
+        img: ""
+    },
+    "antoniojorge10_": {
+        desc: "Antônio Jorge",
+        img: ""
+    },
+    "leticiafeba_": {
+        desc: "Letícia",
+        img: ""
+    },
+    "_manu_marques": {
+        desc: "Manu",
+        img: ""
+    },
+    "anarfls": {
+        desc: "Ana Frozenha",
+        img: ""
+    },
+    "jdeliciaa": {
+        desc: "Jota Delícia",
+        img: ""
+    },
+    "josifurtado6": {
+        desc: "Joyce",
+        img: ""
+    },
+    "mariane": {
+        desc: "Mariane",
+        img: ""
+    },
+    "lud": {
+        desc: "Ludimila",
+        img: ""
+    },
+    "maju": {
+        desc: "Maju",
+        img: ""
+    },
+    "arthur": {
+        desc: "Arthur",
+        img: ""
+    },
+    "joanadarcfurtado_": {
+        desc: "Joana Darc",
+        img: ""
+    },
+    "irli_montelo": {
+        desc: "Irliane",
+        img: ""
+    },
+    "inethypereira": {
+        desc: "Inethy",
+        img: ""
+    },
+    "luiza": {
+        desc: "Luiza",
+        img: ""
+    },
+    "victtineo": {
+        desc: "Lorenzo",
+        img: ""
+    }
 }
